4b864dbbf5bd6c2a6721b883ce8ee92de1d579f7
MarinerT <marino.todd@gmail.com>
2020-05-21 18:41:38 -0400

    Update predict.py

--- projects/p2_image_classifier/predict.py
+++ projects/p2_image_classifier/predict.py
@@ -15,7 +15,7 @@ def Main():
     parser.add_argument('model', help='.h5 file')
     
     #not mandatory arguments
-    parser.add_argument('--top_k', help='integer; the number of top responses',action='store_false', default=5)
+    parser.add_argument('--top_k', help='integer; the number of top responses',action='store_true', default=5)
     parser.add_argument('--category_names', help='a json file; map of label to catetgory',action='store_true',default='./label_map.json')
 
     args = parser.parse_args()
@@ -62,7 +62,7 @@ def Main():
     #outputs
 
     #print the top_k and their associated probabilities
-    if args.top_k:
+    if not args.top_k:
         
         top_k_probs, top_k_indices = tf.math.top_k(prob_predictions, k=args.top_k)
         probs = top_k_probs.numpy().tolist()
