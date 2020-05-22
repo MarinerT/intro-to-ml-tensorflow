bfa515be873821c4b8b35aa24d45b2566e9f7ce3
MarinerT <marino.todd@gmail.com>
2020-05-21 22:15:14 -0400

    Update predict.py

--- projects/p2_image_classifier/predict.py
+++ projects/p2_image_classifier/predict.py
@@ -20,6 +20,25 @@ def Main():
 
     args = parser.parse_args()
     
+    #creating the model
+    #loading the MobileNet from TensorFlow Hub
+    url = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4'
+
+    feature_extractor = hub.KerasLayer(url, input_shape = (image_size, image_size,3))
+    feature_extractor.trainable = False
+
+    # build the model
+    model = tf.keras.Sequential([feature_extractor, tf.keras.layers.Dense(102, activation='softmax')])
+
+    model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy',metrics=['accuracy'])
+
+    #loading weights
+    model.load_weights(args.model)
+
+    #map labels
+    with open(args.category_names,'r') as f:
+        class_names = simplejson.load(f)
+    
     prob_predictions = predict_probs()
     
     if args.top_k:
@@ -41,26 +60,6 @@ def Main():
 
         print(labels[np.argmax(probs)],max(probs))
 
-        
-#creating the model
-#loading the MobileNet from TensorFlow Hub
-url = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4'
-
-feature_extractor = hub.KerasLayer(url, input_shape = (image_size, image_size,3))
-feature_extractor.trainable = False
-
-# build the model
-model = tf.keras.Sequential([feature_extractor, tf.keras.layers.Dense(102, activation='softmax')])
-
-model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy',metrics=['accuracy'])
-
-#loading weights
-model.load_weights(args.model)
-  
-#map labels
-with open(args.category_names,'r') as f:
-    class_names = simplejson.load(f)
-
 #process image
 def process_image(image_path):
     #process image
@@ -75,31 +74,6 @@ def predict_probs():
     predictions = model(process_image(args.path), training=False)
     prob_predictions = predictions[0]
     return prob_predictions
-    
-    #outputs
-
-#     #print the top_k and their associated probabilities
-#     if args.top_k != 5:
-        
-#         top_k_probs, top_k_indices = tf.math.top_k(prob_predictions, k=args.top_k)
-#         probs = top_k_probs.numpy().tolist()
-#         classes = top_k_indices.numpy().tolist()
-#         classes = [n+1 for n in classes]
-#         labels = [class_names[str(n+1)] for n in classes]
-
-#         for _ in range(args.top_k):
-#             print('\t\u2022' + str(probs[_]) + ':' + str(labels[_]))
-            
-#     #print the most likely label & it's associated probability
-#     else:
-#         top_k_probs, top_k_indices = tf.math.top_k(prob_predictions, k=5)
-#         probs = top_k_probs.numpy().tolist()
-#         classes = top_k_indices.numpy().tolist()
-#         classes = [n+1 for n in classes]
-#         labels = [class_names[str(n+1)] for n in classes]
-
-#         print(labels[np.argmax(probs)],max(probs))
 
 if __name__ == '__main__':
     Main()
-    
